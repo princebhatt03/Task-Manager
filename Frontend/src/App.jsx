c147382c025b63caa072ce8155262c82172d7a12
Prince Bhatt <sharmaprince89667@gmail.com>
2025-08-21 17:13:03 +0530

    Task Started, Project Folder Structure Setup Done, Dependencies Installed

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import ErrorPage from './pages/ErrorPage';
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home />}
+        />
+        <Route
+          path="*"
+          element={<ErrorPage />}
+        />
+      </Routes>
+    </>
+  );
+}
+
+export default App;
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../components/Header';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <h1 className="bg-red-500">Home</h1>
+    </div>
+  );
+};
+
+export default Home;
